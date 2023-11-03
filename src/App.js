edfff4510291aaa13589964d50cf9abc56d04e77
Abhishek Sharma <sharma312006@gmail.com>
2023-11-03 14:42:08 +0530

    adding Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <Header />
+      </header>
     </div>
   );
 }
